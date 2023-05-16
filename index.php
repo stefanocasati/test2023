0dccdb6fa82d294d4c37e98da3712333e0eeeeac
stefanocasati <casati.s@gmail.com>
2023-05-16 18:25:39 +0200

    gitignore

--- index.php
+++ index.php
@@ -3,4 +3,5 @@ echo "nuovo index ".date("d-m-y")." ".time().". Oggi è il 16 maggio";
 echo "nuovo repo 2. proviamo un altra modifica";
 echo "ciao";
 echo ". non ci sto capendo molto";
+echo ". proviamo se funziona senza conflitti";
 ?>
