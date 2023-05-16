f6edb32bb19ad6c58826c9e25dc216772cd9a531
stefanocasati <casati.s@gmail.com>
2023-05-16 18:27:57 +0200

    gitignore

--- index.php
+++ index.php
@@ -3,5 +3,5 @@ echo "nuovo index ".date("d-m-y")." ".time().". Oggi è il 16 maggio";
 echo "nuovo repo 2. proviamo un altra modifica";
 echo "ciao";
 echo ". non ci sto capendo molto. questa modifica può generare dei conflitti";
-echo ". proviamo se funziona senza conflitti";
+echo ". proviamo se funziona senza conflitti. mi aspetterei che qeusto generi conflitti";
 ?>
