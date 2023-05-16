0442e3ec758239a6d915f11243dc1f40c25e53a5
stefanocasati <casati.s@gmail.com>
2023-05-16 18:31:57 +0200

    gitignore

--- index.php
+++ index.php
@@ -4,4 +4,9 @@ echo "nuovo repo 2. proviamo un altra modifica";
 echo "ciao";
 echo ". non ci sto capendo molto. questa modifica può generare dei conflitti";
 echo ". proviamo se funziona senza conflitti. mi aspetterei che qeusto generi conflitti";
+echo "<br/>";
+$x = 5;
+$y = 10.5;
+echo "la variabile è ".$x;
+
 ?>
